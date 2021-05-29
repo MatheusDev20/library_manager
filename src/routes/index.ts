dfff4e4e539770c57a096698625a49455c4194b2
Matheus <matheusdev20@gmail.com>
2021-05-29 19:26:18 -0300

    Test deploy

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,7 +3,7 @@ import Router from 'express';
 const router = Router();
 
 router.get('/', (req, res) => {
-  res.json({ message: 'Running' });
+  res.json({ message: 'Testing automatic deploy' });
 });
 
 export default router;
