3e9c43d6776b647c1e0959a64d99f7a093d9aa61
Matheus <matheusdev20@gmail.com>
2021-06-02 02:21:12 -0300

    Teste

--- src/routes/users/usersRoute.ts
+++ src/routes/users/usersRoute.ts
@@ -3,6 +3,11 @@ import { UserController } from '../../presenters/controllers/UserControler';
 
 const userRoutes = Router();
 const userController = new UserController();
+
 userRoutes.post('/', userController.create);
+
+userRoutes.get('/', (req, res) => {
+  res.json({ message: 'Hello World' }).send();
+});
 
 export default userRoutes;
